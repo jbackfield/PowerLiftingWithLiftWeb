82df634b7fcdca482b0a2a29ef03b9a970518f7a
Joshua Backfield <suroot.chmod777@gmail.com>
2016-12-04 19:49:13 -0500

    Initial revision containing basic lift project

---
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -0,0 +1,54 @@
+package bootstrap.liftweb
+
+import java.util.TimeZone
+
+import net.liftweb.common.Full
+import net.liftweb.http.{LiftRules, Req, Html5Properties}
+import net.liftweb.sitemap.{SiteMap, Menu}
+
+object Pages {
+
+  val index = Menu("index") / "index"
+
+}
+
+class Boot {
+
+  def setupSiteMap : Boot = {
+    LiftRules.setSiteMapFunc(() => SiteMap(
+      Pages.index
+    ))
+    this
+  }
+
+  def setupMisc : Boot = {
+    // where to search snippet
+    LiftRules.addToPackages("io.bann.web")
+
+    /*
+     * Show the spinny image when an Ajax call starts
+     */
+    LiftRules.ajaxStart =
+      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
+
+    /*
+     * Make the spinny image go away when it ends
+     */
+    LiftRules.ajaxEnd =
+      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
+
+    LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
+
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+
+    LiftRules.htmlProperties.default.set((r : Req) => new Html5Properties(r.userAgent))
+
+    this
+  }
+
+  def boot {
+    //setupDatabase.setupSiteMap.setupServices.setupMisc
+    setupSiteMap.setupMisc
+  }
+
+}
