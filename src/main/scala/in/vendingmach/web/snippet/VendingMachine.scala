e1dd99ef3a77e8e4dfcb28af47c7c87ff8f14e1b
Joshua Backfield <suroot.chmod777@gmail.com>
2017-01-29 13:53:52 -0500

    Created second snippet

--- src/main/scala/in/vendingmach/web/snippet/VendingMachine.scala
+++ src/main/scala/in/vendingmach/web/snippet/VendingMachine.scala
@@ -1,16 +1,15 @@
 package in.vendingmach.web.snippet
 
-import in.vendingmach.web.model.{SpiteDrink, DietCrudDrink, CrudDrink, Drink}
-import net.liftweb.http.{GUIDJsExp, SHtml}
-import net.liftweb.http.js.{JE, JsCmds, JsCmd}
-import net.liftweb.json.JsonAST.{JString, JArray, JInt}
-import net.liftweb.json.JsonDSL._
-import net.liftweb.json.{JObject, parseOpt}
+import net.liftweb.util._, Helpers._
 
-import scala.xml.NodeSeq
-
-class VendingMachine {
+object VendingMachine {
 
   println("Vending Machine Initialized")
+
+  def helloWorld() : CssSel = {
+    "h1 *" #> "Hello World" &
+      "a *" #> "DevNexus" &
+      "a [href+]" #> "http://devnexus.com"
+  }
 
 }
