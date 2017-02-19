c0a480efa26b26a62d66a336253175ef2fd63b72
Joshua Backfield <suroot.chmod777@gmail.com>
2017-02-19 13:17:22 -0500

    Finished up ajax-param-snippet

--- src/main/scala/in/vendingmach/web/snippet/FooSnippet.scala
+++ src/main/scala/in/vendingmach/web/snippet/FooSnippet.scala
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat
 
 import bootstrap.liftweb.Pages
 import in.vendingmach.web.model.Foo
+import net.liftweb.http.{GUIDJsExp, SHtml}
+import net.liftweb.http.js.JsCmds
 import net.liftweb.util._, Helpers._
 
 class FooSnippet(foo : Foo) {
@@ -11,7 +13,11 @@ class FooSnippet(foo : Foo) {
   val formatter = new SimpleDateFormat()
 
   def body : CssSel = {
+    val GUIDJsExp(_, js) = SHtml.ajaxInvoke(
+      () => JsCmds.Alert(foo.uuid.toString)
+    )
     ".page-link [href+]" #> Pages.foo.calcHref(foo) &
+      "#timestamp [onclick+]" #> js &
       "#timestamp *" #> formatter.format(foo.created.getTime)
   }
 
