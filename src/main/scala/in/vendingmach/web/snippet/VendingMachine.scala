71e5e2d2f1a042b88f5343245477ddb72f053f4a
Joshua Backfield <suroot.chmod777@gmail.com>
2017-02-23 13:45:16 -0500

    Removed extraneous imports.

--- src/main/scala/in/vendingmach/web/snippet/VendingMachine.scala
+++ src/main/scala/in/vendingmach/web/snippet/VendingMachine.scala
@@ -1,14 +1,5 @@
 package in.vendingmach.web.snippet
 
-import in.vendingmach.web.model.{SpiteDrink, DietCrudDrink, CrudDrink, Drink}
-import net.liftweb.http.{GUIDJsExp, SHtml}
-import net.liftweb.http.js.{JE, JsCmds, JsCmd}
-import net.liftweb.json.JsonAST.{JString, JArray, JInt}
-import net.liftweb.json.JsonDSL._
-import net.liftweb.json.{JObject, parseOpt}
-
-import scala.xml.NodeSeq
-
 class VendingMachine {
 
   println("Vending Machine Initialized")
